faecb12786420cb570c72a1b8822a00e5a292158
luizfspintoo <luizfspintoo@gmail.com>
2023-05-07 18:11:42 -0300

    create database

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+  databaseURL: "https://playground-65099-default-rtdb.firebaseio.com/"
+} 
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shopppingListInDB = ref(database, "shoppingList")
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonField = document.getElementById("add-button")
 
 addButtonField.addEventListener("click", function(){
   let inputValue = inputFieldEl
+  push(shopppingListInDB, inputValue)
   console.log(inputValue)
 })
