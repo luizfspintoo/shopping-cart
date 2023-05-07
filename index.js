12e8b121d8fedc181d0a8f90df38fb38272cd99a
luizfspintoo <luizfspintoo@gmail.com>
2023-05-07 16:12:22 -0300

    starting with JavaScript

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputFieldEl = document.getElementById("input-field")
+const addButtonField = document.getElementById("add-button")
+
+addButtonField.addEventListener("click", function(){
+  let inputValue = inputFieldEl
+  console.log(inputValue)
+})
